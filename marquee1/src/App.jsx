c86a903b6c09cc73389f1a0b294ffba245a3945b
mayurkukade <mayurkukade@opticalarc.com>
2023-06-08 15:23:54 +0530

    update

--- marquee1/src/App.jsx
+++ marquee1/src/App.jsx
@@ -1,12 +1,42 @@
-
-
+import Marquee from "react-fast-marquee";
 const App = () => {
   return (
     <>
-     <p>App</p> 
+      <Marquee autoFill speed={15}>
+        <img
+          src="../public/img/pngwing.com (1).png"
+          width={"25px"}
+          height={"25px"}
+        />
+        <img
+          src="../public/img/pngwing.com (2).png"
+          width={"25px"}
+          height={"25px"}
+        />
+        <img
+          src="../public/img/pngwing.com (3).png"
+          width={"25px"}
+          height={"25px"}
+        />
+
+        <img
+          src="../public/img/pngwing.com (4).png"
+          width={"25px"}
+          height={"25px"}
+        />
+        <img
+          src="../public/img/pngwing.com (5).png"
+          width={"25px"}
+          height={"25px"}
+        />
+        <img
+          src="../public/img/pngwing.com (6).png"
+          width={"25px"}
+          height={"25px"}
+        />
+      </Marquee>
     </>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
